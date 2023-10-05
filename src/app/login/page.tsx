db12a073f7e73cf737022c582caefdb5010af63c
Aditya Shrivastav <81470938+aditya-shrivastavv@users.noreply.github.com>
2023-10-05 21:59:56 +0530

    Login page completed

---
+++ src/app/login/page.tsx
@@ -0,0 +1,41 @@
+'use client'
+
+import GoogleLogo from '@/components/logo/GoogleLogo'
+import Button from '@/components/ui/Button'
+import React, { useState } from 'react'
+
+type Props = {}
+
+const Login = (props: Props) => {
+  const [loading, setLoading] = useState<boolean>(false)
+
+  async function loginWithGoogle() {
+    console.log('hehe!!')
+  }
+
+  return (
+    <>
+      <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+        <div className="w-full flex flex-col items-center max-w-md space-y-8">
+          <div className="flex flex-col items-center gap-8">
+            Logo
+            <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
+              Sign in to your account
+            </h2>
+          </div>
+
+          <Button
+            isLoading={loading}
+            type="button"
+            className="max-w-sm mx-auto w-full"
+            onClick={loginWithGoogle}
+          >
+            <GoogleLogo /> Google
+          </Button>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Login
