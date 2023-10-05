2c367ecb950ad874dd9c6c6429a6e500c6948980
Aditya Shrivastav <81470938+aditya-shrivastavv@users.noreply.github.com>
2023-10-05 22:15:21 +0530

    implement toaster

---
+++ src/components/Providers.tsx
@@ -0,0 +1,19 @@
+'use client'
+
+import React from 'react'
+import { Toaster } from 'react-hot-toast'
+
+type Props = {
+  children: React.ReactNode
+}
+
+const Providers = ({ children }: Props) => {
+  return (
+    <>
+      <Toaster position="top-center" reverseOrder={false} />
+      {children}
+    </>
+  )
+}
+
+export default Providers
