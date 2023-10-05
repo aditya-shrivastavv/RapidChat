412208fca71a203ff3362450b7557d8ad48b616f
Aditya Shrivastav <81470938+aditya-shrivastavv@users.noreply.github.com>
2023-10-05 18:40:01 +0530

    db connection successful

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
-import Image from 'next/image'
+import { db } from '@/lib/db'
 
-export default function Home() {
-  return (
-    <h1 className='text-red-500'>Hello</h1>
-  )
+export default async function Home() {
+  await db.set('test', 'test')
+
+  return <h1 className="text-red-500">Hello</h1>
 }
