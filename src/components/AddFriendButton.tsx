287b2d4402ee1b280f0b932c7399da8cd479de40
Aditya Shrivastav <81470938+aditya-shrivastavv@users.noreply.github.com>
2023-10-06 08:17:19 +0530

    creating add friend button

---
+++ src/components/AddFriendButton.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Button from './ui/Button'
+
+type Props = {}
+
+const AddFriendButton = (props: Props) => {
+  return <Button>AddFriendButton</Button>
+}
+
+export default AddFriendButton
