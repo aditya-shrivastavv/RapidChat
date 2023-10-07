afa83168dfcb52ee0a623009fa0bc7c3e043483d
Aditya Shrivastav <81470938+aditya-shrivastavv@users.noreply.github.com>
2023-10-07 17:17:45 +0530

    BROKEN

--- src/helpers/redis.ts
+++ src/helpers/redis.ts
@@ -5,4 +5,18 @@ type Command = 'zrange' | 'sismember' | 'get' | 'smembers'
 
 export async function fetchRedis(command: Command, ...args: (string | number)[]) {
   const commandUrl = `${upstashRedisRestUrl}/${command}/${args.join('/')}`
+
+  const response = await fetch(commandUrl, {
+    headers: {
+      Authorization: `Bearer ${upstashRedisRestToken}`
+    },
+    cache: 'no-store'
+  })
+
+  if (!response.ok) {
+    throw new Error(`Error executing Redis command: ${response.statusText}`)
+  }
+
+  const data = await response.json()
+  return data.result
 }
