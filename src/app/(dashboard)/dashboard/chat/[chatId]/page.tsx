8057ef374290c9dad77e9c3984e28b2c72c9e7c7
Aditya Shrivastav <81470938+aditya-shrivastavv@users.noreply.github.com>
2023-10-10 18:51:42 +0530

    messages component added

--- src/app/(dashboard)/dashboard/chat/[chatId]/page.tsx
+++ src/app/(dashboard)/dashboard/chat/[chatId]/page.tsx
@@ -1,3 +1,4 @@
+import Messages from '@/components/Messages'
 import { fetchRedis } from '@/helpers/redis'
 import { authOptions } from '@/lib/auth'
 import { db } from '@/lib/db'
@@ -70,6 +71,8 @@ const Chat = async ({ params }: Props) => {
           </div>
         </div>
       </div>
+
+      <Messages sessionId={session.user.id} initialMessages={initialMessages} />
     </div>
   )
 }
