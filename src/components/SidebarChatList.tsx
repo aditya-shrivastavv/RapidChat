1956e12562c3edb4db9146b5370a48c5630e6efb
Aditya Shrivastav <81470938+aditya-shrivastavv@users.noreply.github.com>
2023-10-10 14:05:24 +0530

    friends styling in sidebar

--- src/components/SidebarChatList.tsx
+++ src/components/SidebarChatList.tsx
@@ -31,7 +31,17 @@ const SidebarChatList = ({ friends, sessionId }: Props) => {
 
         return (
           <li key={friend.id}>
-            <a href={`/dashboard/chat/${chatHrefConstructor(sessionId, friend.id)}`}>Hi</a>
+            <a
+              href={`/dashboard/chat/${chatHrefConstructor(sessionId, friend.id)}`}
+              className="text-gray-700 hover:text-indigo-600 hover:bg-gray-50 group flex items-center gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold"
+            >
+              {friend.name}
+              {unseenMessageCount > 0 ? (
+                <div className="bg-indigo-600 font-medium text-xs text-white w-4 h-4 rounded-full flex justify-center items-center">
+                  {unseenMessageCount}
+                </div>
+              ) : null}
+            </a>
           </li>
         )
       })}
